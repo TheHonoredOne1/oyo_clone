430a2518cee0d0dff7dfb8967e4f5814ec22e2cc
Sushrut Bawanthade <thehonoredone1@gmail.com>
2024-06-20 15:09:15 +0530

    home UI done.

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='h-28 flex justify-center items-center text-2xl bg-gray-300'>
+            All rights are reserved &copy; 2024.
+        </div>
+    )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,25 @@ import Header1 from '@/components/Header1'
 import Header2 from '@/components/Header2'
 import Header3 from '@/components/Header3'
 import Header4 from '@/components/Header4'
+import Footer from '@/components/Footer'
 import Head from 'next/head'
 import Image from 'next/image'
+// import Footer from '@/components/Footer'
 const Home = () => {
   return (
     <div>
-    
-      
+
+
       <Head>
         <title>OYO : India`s best online hotel Booking Site For Sanitized Stay </title>
         <link rel="icon" href="/icon.png"></link>
       </Head>
-      
+
       <Header1 />
       <Header2 />
       <Header3 />
 
       <div className='mx-20'>
-
         <div className='my-14'>
           <Image src={'/banner1.avif'} alt='banner' width={200} height={200} className='w-full h-80' />
         </div>
@@ -28,12 +29,9 @@ const Home = () => {
         <div className='mb-14'>
           <Image src={'/banner2.avif'} alt='banner' width={200} height={200} className='w-full h-40' />
         </div>
-
         <Header4 />
-
-
       </div>
-
+      <Footer />
 
     </div>
   )
